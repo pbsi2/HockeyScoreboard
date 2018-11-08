c3dd60dbad93fcf380a64de10596dd8032fa207a
Pierre B. Samson <pierre.b.samson@gmail.com>
2018-11-07 21:42:09 -0500

    Third iteration

--- app/src/main/java/com/pbsi2/hockeyscoreboard/MainActivity.java
+++ app/src/main/java/com/pbsi2/hockeyscoreboard/MainActivity.java
@@ -3,6 +3,7 @@ package com.pbsi2.hockeyscoreboard;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Chronometer;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -16,7 +17,14 @@ public class MainActivity extends AppCompatActivity {
     private Chronometer periodChronometer;
     private TextView display;
     private ProgressBar progressBar;
+    private ProgressBar progressBar2;
+    private ProgressBar progressBar3;
     private boolean isStart;
+    private boolean isFirstPeriod;
+    private boolean isSecondPeriod;
+    private boolean isThirdPeriod;
+    private int homeScore = 0;
+    private int visitorScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,7 +84,68 @@ public class MainActivity extends AppCompatActivity {
         }
     }*/
 
+    private void displayHomeScore(int number) {
+        TextView homeScoreTextView = findViewById(R.id.team1_score);
+        homeScoreTextView.setText("" + number);
+    }
 
+    public void homeScored(View view) {
+
+        homeScore += 1;
+        displayHomeScore(homeScore);
+    }
+
+    public void substracthomeScored(View view) {
+        int number = 1;
+        if (homeScore > 0)
+            homeScore -= number;
+        displayHomeScore(homeScore);
+    }
+
+    private void displayVisitorScore(int number) {
+        TextView homeScoreTextView = findViewById(R.id.team2_score);
+        homeScoreTextView.setText("" + number);
+    }
+
+    public void visitorScored(View view) {
+
+        visitorScore += 1;
+        displayVisitorScore(visitorScore);
+    }
+
+    public void substractVisitorScored(View view) {
+        int number = 1;
+        if (visitorScore > 0)
+            visitorScore -= number;
+        displayVisitorScore(visitorScore);
+    }
+
+    public void litFirstPeriod(View v) {
+        isFirstPeriod = true;
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(40);
+    }
+
+    public void litSecondPeriod(View v) {
+        isFirstPeriod = true;
+        isSecondPeriod = true;
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(40);
+        progressBar2 = findViewById(R.id.progressBar2);
+        progressBar2.setProgress(40);
+    }
+
+    public void litThirdPeriod(View v) {
+        isFirstPeriod = true;
+        isSecondPeriod = true;
+        isThirdPeriod = true;
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(40);
+        progressBar2 = findViewById(R.id.progressBar2);
+        progressBar2.setProgress(40);
+        progressBar3 = findViewById(R.id.progressBar3);
+        progressBar3.setProgress(40);
+    }
 }
 
 
