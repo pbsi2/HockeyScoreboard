dfe2301693653b01ab618163a5693730819e73f4
Pierre B. Samson <pierre.b.samson@gmail.com>
2018-11-08 21:53:26 -0500

    Fifth iteration

--- app/src/main/java/com/pbsi2/hockeyscoreboard/MainActivity.java
+++ app/src/main/java/com/pbsi2/hockeyscoreboard/MainActivity.java
@@ -9,17 +9,16 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
+
 public class MainActivity extends AppCompatActivity {
 
-    long displayTime;
-    String currentTimeLeft = "20:00";
-    long lcurrentTimeLeft;
+
     private Chronometer periodChronometer;
-    private TextView display;
     private ProgressBar progressBar;
     private ProgressBar progressBar2;
     private ProgressBar progressBar3;
-    private boolean isStart;
+    private boolean isStart = false;
+
     private boolean isFirstPeriod;
     private boolean isSecondPeriod;
     private boolean isThirdPeriod;
@@ -31,58 +30,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //display = findViewById(R.id.t1_pen4_textview);
-        //progressBar = findViewById(R.id.progressBar);
-        //periodChronometer = findViewById(R.id.time_left_textview);
+        periodChronometer = findViewById(R.id.time_toplay_textview);
+//        periodChronometer.setBase(60 * 2 * 1000 * -1);
+//        periodChronometer.setFormat("MM:SS");
+        periodChronometer.setCountDown(true);
 
-        //periodChronometer.setBase(60 * 2 * 1000);
-        //periodChronometer.setFormat("MM:SS");
-        //periodChronometer.setCountDown(true);
-
-
-/*        periodChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+        periodChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometerChanged) {
-                periodChronometer.setBase(60 * 2 * 1000);
-                Toast.makeText(MainActivity.this, "Started 1:" + currentTimeLeft, Toast.LENGTH_SHORT).show();
-                display.setText("HH" + currentTimeLeft);
-                String[] data = currentTimeLeft.split(":");
-                //int minutes = Integer.getInteger(data[0]);
-                //int seconds = Integer.getInteger(data[1]);
-                currentTimeLeft = periodChronometer.getText().toString();
-                Toast.makeText(MainActivity.this, "Started:" + currentTimeLeft, Toast.LENGTH_SHORT).show();
-                display.setText(currentTimeLeft);
-                if (currentTimeLeft.contentEquals("01:13")) {
-                    Toast.makeText(MainActivity.this, "time reached", Toast.LENGTH_SHORT).show();
-                    chronometerChanged.stop();
-
-                }
-                if (currentTimeLeft.contains(":00") || currentTimeLeft.contains(":30")) {
-                    Toast.makeText(MainActivity.this, "Second reached", Toast.LENGTH_SHORT).show();
-                    displayTime += 1;
-                    progressBar.setProgress((int) displayTime);
-                }
                 periodChronometer = chronometerChanged;
             }
+        });
 
-        }); */
 
     }
 
-    /*public void startStopChrono(View v) {
+    public void toggleChrono(View view) {
 
         //periodChronometer.setCountDown(true);
         if (isStart) {
             periodChronometer.stop();
             isStart = false;
-            ((Button) v).setText("Start");
+
         } else {
-            //periodChronometer.setBase((long) 60 * 2 * 1000);
+            periodChronometer.setBase(((long) 60 * 2 * 1000));
+//            - SystemClock.elapsedRealtime()
             periodChronometer.start();
             isStart = true;
-            ((Button) v).setText("Stop");
+
         }
-    }*/
+    }
 
     private void displayHomeScore(int number) {
         TextView homeScoreTextView = findViewById(R.id.team1_score);
@@ -127,24 +104,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void litSecondPeriod(View v) {
-        isFirstPeriod = true;
-        isSecondPeriod = true;
-        progressBar = findViewById(R.id.progressBar);
-        progressBar.setProgress(40);
-        progressBar2 = findViewById(R.id.progressBar2);
-        progressBar2.setProgress(40);
+        if (isFirstPeriod) {
+            isSecondPeriod = true;
+            progressBar = findViewById(R.id.progressBar);
+            progressBar.setProgress(40);
+            progressBar2 = findViewById(R.id.progressBar2);
+            progressBar2.setProgress(40);
+        }
     }
 
     public void litThirdPeriod(View v) {
-        isFirstPeriod = true;
-        isSecondPeriod = true;
-        isThirdPeriod = true;
-        progressBar = findViewById(R.id.progressBar);
-        progressBar.setProgress(40);
-        progressBar2 = findViewById(R.id.progressBar2);
-        progressBar2.setProgress(40);
-        progressBar3 = findViewById(R.id.progressBar3);
-        progressBar3.setProgress(40);
+
+        if (isSecondPeriod) {
+            isThirdPeriod = true;
+            progressBar = findViewById(R.id.progressBar);
+            progressBar.setProgress(40);
+            progressBar2 = findViewById(R.id.progressBar2);
+            progressBar2.setProgress(40);
+            progressBar3 = findViewById(R.id.progressBar3);
+            progressBar3.setProgress(40);
+        }
     }
 }
 
